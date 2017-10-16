71eb9945eb8f45dbcf6547a34c024970f45d0e5d
John <16932417+torrencj@users.noreply.github.com>
2017-10-16 14:43:58 -0500

    Initial commit

---
+++ pyserve.py
@@ -0,0 +1,26 @@
+#!flask/bin/python
+
+from flask import Flask
+from pyvirtualdisplay import Display
+from selenium import webdriver
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/api/<siteURL>', methods=['GET'])
+def get_pic(siteURL):
+  display = Display(visible=0, size=(1024,800))
+  display.start()
+
+  print "Starting display..."
+  browser = webdriver.Firefox()
+  print "Loading site...."
+  browser.get('https://' + siteURL)
+  browser.get_screenshot_as_base64('screenie.txt')
+  return "Saved"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,1 @@
+# pyserve
