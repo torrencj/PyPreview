e2a93e27fe174d15e8e653e2da3a61cfab0ae644
John <16932417+torrencj@users.noreply.github.com>
2017-11-14 13:56:52 -0600

    added latest work

--- pyserve.py
+++ pyserve.py
@@ -11,16 +11,17 @@ def index():
     return "Hello, World!"
 
 @app.route('/api/<siteURL>', methods=['GET'])
-def get_pic(siteURL):
-  display = Display(visible=0, size=(1024,800))
-  display.start()
-
-  print "Starting display..."
-  browser = webdriver.Firefox()
-  print "Loading site...."
-  browser.get('https://' + siteURL)
-  browser.get_screenshot_as_base64('screenie.txt')
-  return "Saved"
+return "Hello " + siteURL
+# def get_pic(siteURL):
+#   display = Display(visible=0, size=(1024,800))
+#   display.start()
+#
+#   print "Starting display..."
+#   browser = webdriver.Firefox()
+#   print "Loading site...."
+#   browser.get('https://' + siteURL)
+#   browser.get_screenshot_as_base64('screenie.txt')
+#   return "Saved"
 
 if __name__ == '__main__':
     app.run(debug=True)
