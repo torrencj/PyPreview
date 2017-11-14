474d1e34530472694a5670fa1f9441414e8a8703
John <16932417+torrencj@users.noreply.github.com>
2017-11-14 14:46:35 -0600

    added requirement

--- app.py
+++ app.py
@@ -10,18 +10,17 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-# @app.route('/api/<siteURL>', methods=['GET'])
-# return "Hello " + siteURL
-# def get_pic(siteURL):
-#   display = Display(visible=0, size=(1024,800))
-#   display.start()
-#
-#   print "Starting display..."
-#   browser = webdriver.Firefox()
-#   print "Loading site...."
-#   browser.get('https://' + siteURL)
-#   browser.get_screenshot_as_base64('screenie.txt')
-#   return "Saved"
+@app.route('/api/<siteURL>', methods=['GET'])
+def get_pic(siteURL):
+    # return "Hello " + siteURL
+  display = Display(visible=0, size=(1024,800))
+  display.start()
+  print "Starting display..."
+  browser = webdriver.Firefox()
+  print "Loading site...."
+  browser.get('https://' + siteURL)
+  browser.get_screenshot_as_base64('screenie.txt')
+  return "Saved"
 
 if __name__ == '__main__':
     app.run(debug=True)
