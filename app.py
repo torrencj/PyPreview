a438fe614108dd2f603995318235bebf9fd2bb99
John <16932417+torrencj@users.noreply.github.com>
2017-11-16 13:22:19 -0600

    removed testing in /api/

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ def index():
 
 @app.route('/api/<siteURL>', methods=['GET'])
 def get_pic(siteURL):
-    return "Hello " + siteURL
-  # display = Display(visible=0, size=(1024,800))
-  # display.start()
-  # print("Starting display...")
-  # browser = webdriver.Firefox()
-  # print("Loading site....")
-  # browser.get('https://' + siteURL)
-  # browser.get_screenshot_as_base64('screenie.txt')
-  # return "Saved"
+    # return "Hello " + siteURL
+  display = Display(visible=0, size=(1024,800))
+  display.start()
+  print("Starting display...")
+  browser = webdriver.Firefox()
+  print("Loading site....")
+  browser.get('https://' + siteURL)
+  browser.get_screenshot_as_base64('screenie.txt')
+  return "Saved"
 
 if __name__ == '__main__':
     app.run(debug=True)
