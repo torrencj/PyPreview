b393f948be8561dce48c09535040762e2029f81f
John <16932417+torrencj@users.noreply.github.com>
2017-11-16 14:32:39 -0600

    switched to chrome-stable from google-chrome

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_pic(siteURL):
   display = Display(visible=0, size=(1024,800))
   display.start()
   print("Starting display...")
-  browser = webdriver.Chrome("/app/.apt/usr/bin/google-chrome") #Chromedriver is installed in a non default dir https://github.com/heroku/heroku-buildpack-xvfb-google-chrome
+  browser = webdriver.Chrome("/app/.apt/usr/bin/google-chrome-stable") #Chromedriver is installed in a non default dir https://github.com/heroku/heroku-buildpack-xvfb-google-chrome
   print("Loading site....")
   browser.get('https://' + siteURL)
   browser.get_screenshot_as_base64('screenie.txt')
