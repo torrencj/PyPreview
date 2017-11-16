b09746c9c0227ae8ad3302674329c5964ae73d5f
John <16932417+torrencj@users.noreply.github.com>
2017-11-16 14:53:58 -0600

    trying to connect to chromedriver service

--- app.py
+++ app.py
@@ -13,14 +13,15 @@ def index():
 @app.route('/api/<siteURL>', methods=['GET'])
 def get_pic(siteURL):
     # return "Hello " + siteURL
-  display = Display(visible=0, size=(1024,800))
-  display.start()
-  print("Starting display...")
-  browser = webdriver.Chrome("/app/.apt/usr/bin/google-chrome-stable") #Chromedriver is installed in a non default dir https://github.com/heroku/heroku-buildpack-xvfb-google-chrome
-  print("Loading site....")
-  browser.get('https://' + siteURL)
-  browser.get_screenshot_as_base64('screenie.txt')
-  return "Saved"
+    print("Starting display...")
+    display = Display(visible=0, size=(1024,800))
+    display.start()
+    print("Loading site....")
+    browser = webdriver.Chrome("/app/.apt/usr/bin/google-chrome-stable") #Chromedriver is installed in a non default dir https://github.com/heroku/heroku-buildpack-xvfb-google-chrome
+    browser.start()
+    browser.get('https://' + siteURL)
+    browser.get_screenshot_as_base64('screenie.txt')
+    return "Saved"
 
 if __name__ == '__main__':
     app.run(debug=True)
